04b8bc842bcead6906adb2942f1b1718473acca6
au1hor <authorestar@gmail.com>
2025-05-29 16:15:53 -0300

    DIA - 1 - TARDE
    ta girando e trocando de posição quando aperto espaço alem de aumentar a velocidae ele checka se ta no range do acerto tem mt oq melhorar

--- ShavedBalls/Assets/RangeMove.cs
+++ ShavedBalls/Assets/RangeMove.cs
@@ -1,8 +1,17 @@
+using System;
+
+using Unity.Mathematics;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class RangeMove : MonoBehaviour
 {
-    
+    public GameObject range2;
+    public Boolean apertou = false;
+    public int dir = 1;
+    public float speed = 10;
+    private float dif = 5;
+    private float rotaZ;
     void Start()
     {
         
@@ -11,7 +20,26 @@ public class RangeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        rotation = Quaternion.Euler();
-        
+       
+        Vector3 euler1 = transform.eulerAngles;
+        Vector3 eulerRange = range2.transform.rotation.eulerAngles;
+        Vector3 distance = euler1 - eulerRange;
+        rotaZ = distance.z;
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            dir *= -1;
+            apertou = true;
+            speed += dif;
+            if (Mathf.Abs(rotaZ) < 10 )
+            {
+                Debug.Log("acertou");
+                Debug.Log(rotaZ);
+            }
+        }
+        else
+        {
+            apertou = false;
+        }
+        transform.Rotate(0,0,speed * Time.deltaTime * dir);
     }
 }
