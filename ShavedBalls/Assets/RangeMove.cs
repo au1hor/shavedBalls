4f5b826a50612d48d08ade5988005ca0668c2a70
au1hor <authorestar@gmail.com>
2025-05-29 15:12:37 -0300

    DS
    DS

---
+++ ShavedBalls/Assets/RangeMove.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class RangeMove : MonoBehaviour
+{
+    
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rotation = Quaternion.Euler();
+        
+    }
+}
