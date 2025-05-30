f2f35c192af00f81f714b1c5d3b6c29add993de1
au1hor <authorestar@gmail.com>
2025-05-30 13:04:17 -0300

    Dia - 2 - manha
    MENU START
    - TROCA DA ROTAÇÃO DO TARGET
    - PONTOS ADICIONADOS
    - VIDAS ADICIONADOS
    - ANIMAÇÃO 50% COMPLETA PERDA DE VIDA
    *ARRUMAR o RANGE DE ACERTO

---
+++ ShavedBalls/Assets/Manger.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Manger : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Button IniciarButton;
+    public Canvas MenuInicial;
+    public GameObject gameplay;
+    void Start()
+    {
+        IniciarButton.onClick.AddListener(ClickInicial);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    void ClickInicial()
+    {
+        MenuInicial.gameObject.SetActive(false);
+        gameplay.gameObject.SetActive(true);
+
+    }
+}
--- ShavedBalls/Assets/RangeMove.cs
+++ ShavedBalls/Assets/RangeMove.cs
@@ -1,17 +1,31 @@
 using System;
-
+using TMPro;
 using Unity.Mathematics;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class RangeMove : MonoBehaviour
 {
     public GameObject range2;
+    public Text PointsText;
     public Boolean apertou = false;
+    public int points = 0;
     public int dir = 1;
     public float speed = 10;
     private float dif = 5;
     private float rotaZ;
+    private int lifes = 3;
+
+    public TextMeshProUGUI decressLife;
+    public Text lifesTexts;
+    private float speedTarget = 30;
+    public RectTransform decressLifeRect;
+    public CanvasGroup canvasGroup;
+    public float duracao = 1f;
+    public float distancia = 5f;
+
     void Start()
     {
         
@@ -20,7 +34,12 @@ public class RangeMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-       
+        if (lifes == 0)
+        {
+            GameRestart();
+            return;
+        }
+        PointsText.text = points.ToString();
         Vector3 euler1 = transform.eulerAngles;
         Vector3 eulerRange = range2.transform.rotation.eulerAngles;
         Vector3 distance = euler1 - eulerRange;
@@ -30,16 +49,57 @@ public class RangeMove : MonoBehaviour
             dir *= -1;
             apertou = true;
             speed += dif;
-            if (Mathf.Abs(rotaZ) < 10 )
+            if (Mathf.Abs(rotaZ) < 20)
             {
-                Debug.Log("acertou");
-                Debug.Log(rotaZ);
+                NextRange();
+            }
+            else
+            {
+                decressLife.gameObject.SetActive(true);
+                lifes -= 1;
+                canvasGroup.alpha = 1;
+                decressLife.text = "-1";
+                StartCoroutine(Animar());
+                lifesTexts.text = "Vidas =  " + lifes;
             }
         }
         else
         {
             apertou = false;
         }
-        transform.Rotate(0,0,speed * Time.deltaTime * dir);
+        transform.Rotate(0, 0, speed * Time.deltaTime * dir);
+    }
+
+    void NextRange()
+    {
+        points += 1;
+        Debug.Log("oieee");
+        speedTarget = UnityEngine.Random.Range(0, 100);
+        range2.transform.Rotate(0, 0, speedTarget);
+        PointsText.text = points.ToString();
+    }
+    void GameRestart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    private System.Collections.IEnumerator Animar()
+    {
+        Vector2 posInicial = decressLifeRect.anchoredPosition;
+        Vector2 posDest = posInicial - new Vector2(0, distancia);
+        float tempo = 0;
+        while (tempo < duracao)
+        {
+            tempo += Time.deltaTime;
+            float t = tempo / duracao;
+
+            decressLifeRect.anchoredPosition = Vector2.Lerp(posInicial, posDest, t);
+            canvasGroup.alpha = Mathf.Lerp(1, 0, t);
+            yield return null;
+        }
+        decressLifeRect.anchoredPosition = posDest;
+        canvasGroup.alpha = 0;
+        decressLifeRect.anchoredPosition = posInicial;
+        decressLife.gameObject.SetActive(false);
+        
     }
 }
