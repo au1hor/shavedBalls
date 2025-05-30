84118cee9eb26fed9e15a4a7675b8fb88a963db5
au1hor <authorestar@gmail.com>
2025-05-30 15:54:43 -0300

    - DIA - 2 - TARDE
    - ANIMAR os pontos 50%
    * tem que arrumar n ta subindo e ta desaparecendo

--- ShavedBalls/Assets/RangeMove.cs
+++ ShavedBalls/Assets/RangeMove.cs
@@ -9,7 +9,7 @@ using UnityEngine.UI;
 public class RangeMove : MonoBehaviour
 {
     public GameObject range2;
-    public Text PointsText;
+    public TextMeshProUGUI PointsText;
     public Boolean apertou = false;
     public int points = 0;
     public int dir = 1;
@@ -23,12 +23,17 @@ public class RangeMove : MonoBehaviour
     private float speedTarget = 30;
     public RectTransform decressLifeRect;
     public CanvasGroup canvasGroup;
+    public RectTransform PointsRect;
+    public CanvasGroup PointsCanvasGroup;
+
     public float duracao = 1f;
+     public float duracao2 = 1f;
     public float distancia = 5f;
+    public float distancia2 = -5f;
 
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
@@ -51,7 +56,9 @@ public class RangeMove : MonoBehaviour
             speed += dif;
             if (Mathf.Abs(rotaZ) < 20)
             {
+                PointsRect.gameObject.SetActive(true);
                 NextRange();
+                StartCoroutine(AnimarPoints());
             }
             else
             {
@@ -59,7 +66,7 @@ public class RangeMove : MonoBehaviour
                 lifes -= 1;
                 canvasGroup.alpha = 1;
                 decressLife.text = "-1";
-                StartCoroutine(Animar());
+                StartCoroutine(AnimarLife());
                 lifesTexts.text = "Vidas =  " + lifes;
             }
         }
@@ -82,7 +89,7 @@ public class RangeMove : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    private System.Collections.IEnumerator Animar()
+    private System.Collections.IEnumerator AnimarLife()
     {
         Vector2 posInicial = decressLifeRect.anchoredPosition;
         Vector2 posDest = posInicial - new Vector2(0, distancia);
@@ -100,6 +107,27 @@ public class RangeMove : MonoBehaviour
         canvasGroup.alpha = 0;
         decressLifeRect.anchoredPosition = posInicial;
         decressLife.gameObject.SetActive(false);
+
+    }
+    private System.Collections.IEnumerator AnimarPoints()
+    {
+        Vector2 inicialPos = PointsRect.anchoredPosition;
+
+        Vector2 pointsDir = inicialPos - new Vector2(0, distancia2);
+        float tempo2 = 0;
+        while (tempo2 < duracao2)
+        {
+            tempo2 += Time.deltaTime;
+            float t2 = tempo2 / duracao2;
+
+            decressLifeRect.anchoredPosition = Vector2.Lerp(inicialPos, pointsDir, t2);
+            PointsCanvasGroup.alpha = Mathf.Lerp(1, 0, t2);
+            yield return null;
+        }
+        PointsRect.anchoredPosition = pointsDir;
+        canvasGroup.alpha = 1;
+        PointsRect.anchoredPosition = inicialPos;
+        PointsRect.gameObject.SetActive(false);
         
     }
 }
