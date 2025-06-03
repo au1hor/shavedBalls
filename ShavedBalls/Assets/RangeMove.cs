43e9abbe6d02455ae530eae71a19e898c5cd9b67
au1hor <authorestar@gmail.com>
2025-06-03 00:16:42 -0300

    - DIa 5 - tarde
    *postando agora pq tava atrasado*
    + Sistema de cores de acordo com o combo
    + som ao acertar a barra
    + modularização de alguns codigos
    
    ! arrumar e deixar mais limpo o codigo e legivel som pra o aumento de comobo
    !jogo mais bonito

--- ShavedBalls/Assets/RangeMove.cs
+++ ShavedBalls/Assets/RangeMove.cs
@@ -22,7 +22,7 @@ public class RangeMove : MonoBehaviour
     public Text lifesTexts;
     private float speedTarget = 30;
     public RectTransform decressLifeRect;
-    public CanvasGroup canvasGroup;
+    public CanvasGroup LifeCanvasGroup;
     public RectTransform PointsRect;
     public CanvasGroup PointsCanvasGroup;
 
@@ -30,22 +30,29 @@ public class RangeMove : MonoBehaviour
     public RectTransform comboRect;
     public CanvasGroup comboGroup;
 
-    public float duracao = 1f;
-    public float duracao2 = 0.1f;
-    public float distancia = 2f;
-    public float distancia2 = 10f;
+
+    public float duracaoLife = 1f;
+    public float distanciaLife = 2f;
+    public float duracaoPoints = 1f;
+    public float distanciaPoints = 2f;
+    public float duracaoCombo = 0.1f;
+    public float distanciaCombo = 10f;
 
     public int pointsContinus = 0;
 
+    public AudioSource audioSource;
+    public AudioClip soundEfect;
+    public string NomeBrabo = "normal";
+
     void Start()
     {
-
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        comboText.text = "INSANE "+ System.Environment.NewLine + pointsContinus;
+        comboText.text = NomeBrabo + System.Environment.NewLine + pointsContinus;
         if (lifes == 0)
         {
             GameRestart();
@@ -55,7 +62,7 @@ public class RangeMove : MonoBehaviour
         Vector3 euler1 = transform.eulerAngles;
         Vector3 eulerRange = range2.transform.rotation.eulerAngles;
         Vector3 distance = euler1 - eulerRange;
-        rotaZ = Mathf.DeltaAngle(euler1.z,eulerRange.z);
+        rotaZ = Mathf.DeltaAngle(euler1.z, eulerRange.z);
         if (Input.GetKeyDown(KeyCode.Space))
         {
             dir *= -1;
@@ -65,18 +72,18 @@ public class RangeMove : MonoBehaviour
             {
                 PointsRect.gameObject.SetActive(true);
                 NextRange();
-                StartCoroutine(AnimarPoints());
+                StartCoroutine(Animar(PointsRect,PointsCanvasGroup,duracaoCombo,distanciaCombo,false));
                 StartCoroutine(AnimarCombo());
 
             }
             else
             {
-                 comboText.gameObject.SetActive(false);
+                comboText.gameObject.SetActive(false);
                 decressLife.gameObject.SetActive(true);
                 lifes -= 1;
-                canvasGroup.alpha = 1;
+                LifeCanvasGroup.alpha = 1;
                 decressLife.text = "-1";
-                StartCoroutine(AnimarLife());
+                StartCoroutine(Animar(decressLifeRect,LifeCanvasGroup,duracaoLife,distanciaLife,true));
                 lifesTexts.text = "Vidas =  " + lifes;
                 pointsContinus = 0;
             }
@@ -93,21 +100,47 @@ public class RangeMove : MonoBehaviour
         if (pointsContinus > 1)
         {
             points += 1 * pointsContinus;
-        }else
+        }
+        else
         {
             points += 1;
         }
-        
+
         pointsContinus += 1;
         speedTarget = UnityEngine.Random.Range(0, 100);
         range2.transform.Rotate(0, 0, speedTarget);
         PointsText.text = points.ToString();
+        string cor;
+        if (pointsContinus > 10 && pointsContinus < 20)
+        {
+            cor = "verdeClaro";
+        }
+        else if (pointsContinus > 19 && pointsContinus <= 29)
+        {
+            cor = "azulClaro";
+        }
+        else if (pointsContinus >= 30 && pointsContinus < 49)
+        {
+            cor = "amareloClaro";
+
+        }
+        else if (pointsContinus >= 49)
+        {
+            cor = "vermelho";
+        }
+        else
+        {
+            cor = "branco";
+        }
+
+        MudarCor(comboText, cor);
+        audioSource.PlayOneShot(soundEfect);
     }
     void GameRestart()
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
-    private System.Collections.IEnumerator AnimarLife()
+    /*private System.Collections.IEnumerator AnimarLife()
     {
         Vector2 posInicial = decressLifeRect.anchoredPosition;
         Vector2 posDest = posInicial - new Vector2(0, distancia);
@@ -127,7 +160,36 @@ public class RangeMove : MonoBehaviour
         decressLife.gameObject.SetActive(false);
 
     }
-    private System.Collections.IEnumerator AnimarPoints()
+    */
+    private System.Collections.IEnumerator Animar(RectTransform rectTransform, CanvasGroup canvasGroup, float duracao, float distancia, bool alphaMode, GameObject gameObject = null)
+    {
+        Vector2 inicialPos = rectTransform.anchoredPosition;
+        Vector2 posDest = inicialPos - new Vector2(0, distancia);
+        float tempo = 0;
+        while (tempo < duracao)
+        {
+            tempo += Time.deltaTime;
+            float t = tempo / duracao;
+            rectTransform.anchoredPosition = Vector2.Lerp(inicialPos, posDest, t);
+            if (alphaMode)
+            {
+                canvasGroup.alpha = Mathf.Lerp(1, 0, t);
+            }
+            yield return null;
+
+        }
+        rectTransform.anchoredPosition = inicialPos;
+        if (alphaMode)
+        {
+            canvasGroup.alpha = 0;
+
+        }
+        if (gameObject != null)
+        {
+            gameObject.SetActive(false);
+        }
+     }
+/*    private System.Collections.IEnumerator AnimarPoints()
     {
         Vector2 inicialPos = PointsRect.anchoredPosition;
         Vector2 pointsDir = inicialPos - new Vector2(0, distancia2);
@@ -143,17 +205,11 @@ public class RangeMove : MonoBehaviour
         }
         PointsRect.anchoredPosition = inicialPos;
     }
-    /*
-    declarar a posição atual do alvo coo um vector 2d
-    declarar o destino do alvo pela diferença entre a posição inicial e a distancia que quer chegar x,y como um vector 2d
-    declarara a variavel do tempo
-    enquanto o tempo declarado for menor que a  duração 
-    aumentar o tempo com o deltatime
-
     */
     private System.Collections.IEnumerator AnimarCombo()
+
     {
-        
+
         if (pointsContinus > 1)
         {
             comboText.gameObject.SetActive(true);
@@ -162,13 +218,13 @@ public class RangeMove : MonoBehaviour
             float fontInicialSize = comboText.fontSize;
             float fontTamanhoMaximo = 79f;
             Vector2 inicialPosCombo = comboRect.anchoredPosition;
-            Vector2 destinoCombo = inicialPosCombo - new Vector2(distancia2, distancia2);
+            Vector2 destinoCombo = inicialPosCombo - new Vector2(distanciaCombo, distanciaCombo);
 
             float tempo3 = 0;
-            while (tempo3 < duracao2)
+            while (tempo3 < duracaoCombo)
             {
                 tempo3 += Time.deltaTime;
-                float t3 = tempo3 / duracao2;
+                float t3 = tempo3 / duracaoCombo;
                 comboRect.anchoredPosition = Vector2.Lerp(inicialPosCombo, destinoCombo, t3);
                 comboText.fontSize = Mathf.Lerp(fontInicialSize, fontTamanhoMaximo, t3);
                 yield return null;
@@ -177,6 +233,45 @@ public class RangeMove : MonoBehaviour
             comboText.fontSize = fontInicialSize;
 
         }
-       
+
     }
+    public Color HexToColor(string Hex)
+    {
+        Color cor; // declarar a varialvel cor
+        if (UnityEngine.ColorUtility.TryParseHtmlString(Hex, out cor)) // função da unity de conversão hex/html sera armazenada na cor
+        {
+            return cor;
+        }
+        return Color.white; 
+    }
+
+    void MudarCor(  TextMeshProUGUI texto, string Cor)
+    {
+        
+        switch (Cor)
+        {
+            case "verdeClaro":
+                texto.color = HexToColor("#99e599");
+                NomeBrabo = "bom";
+                break;
+            case "azulClaro":
+                texto.color = HexToColor("#4b90e3");
+                NomeBrabo = "medio";
+                break;
+
+            case "amareloClaro":
+                texto.color = HexToColor("#e5e971");
+                NomeBrabo = "anomalia";
+                break;
+
+            case "vermelho":
+                texto.color = HexToColor("#913217");
+                NomeBrabo = "Galado1010";
+                break;
+
+            default:
+                texto.color = Color.white;
+                break;
+        }
+     }
 }
